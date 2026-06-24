730aea198fee92b72b8aae86c58704d0ee72afaf
graes1234 <117164702+graes1234@users.noreply.github.com>
2026-06-24 13:30:23 +0900

    Add import statements to scenarios.$id.tsx

--- src/routes/scenarios.$id.tsx
+++ src/routes/scenarios.$id.tsx
@@ -1,3 +1,4 @@
+
 import { createFileRoute, Link, useParams } from "@tanstack/react-router";
 import { useQuery } from "@tanstack/react-query";
 import { ArrowRight, ClipboardList, Users, Clock, Scale, AlertTriangle } from "lucide-react";
